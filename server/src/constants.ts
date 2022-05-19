5dbdd371e1cdc15b333eb864df7ee25ab2567ef2
Miguel Fernandes <migamado01@gmail.com>
2022-05-19 09:53:43 -0400

    Forgot password token prefix const

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -3,3 +3,6 @@ export const __prod__ = process.env.NODE_ENV === 'production';
 
 // const for name of cookie
 export const COOKIE_NAME = "qid";
+
+// const for the prefix for forget password
+export const FORGET_PASSWORD_PREFIX = 'forget-password:';

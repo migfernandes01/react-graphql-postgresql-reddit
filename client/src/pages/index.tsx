86d1e35d5af603f14730a31ea5555f7042e1152d
Miguel Fernandes <migamado01@gmail.com>
2022-05-24 19:16:44 -0400

    Add link to create post

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,21 +1,26 @@
 import { withUrqlClient } from "next-urql";
+import { Layout } from "../components/Layout";
 import { NavBar } from "../components/NavBar";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import {Link as ChakraLink} from '@chakra-ui/react';
+import Link from 'next/link';
 
 const Index = () => {
   // hook for posts query
   const [{data}] = usePostsQuery();
 
   return (
-    <>
-      <NavBar />
+    <Layout>
+      <Link href="/create-post">
+        <ChakraLink>Create Post</ChakraLink>
+      </Link>
       <div color="white">Hello world</div>
       <br />
       {!data ? <p>Loading...</p> : data.posts.map((post) => (
         <div key={post.id}>{post.title}</div>
       ))}
-    </>
+    </Layout>
   );
   
 };

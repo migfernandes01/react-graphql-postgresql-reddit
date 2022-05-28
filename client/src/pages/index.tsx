9c7bf623444103fa17fc13c6e9ef079c51b0cbd6
Miguel Fernandes <migamado01@gmail.com>
2022-05-28 11:25:54 -0400

    Conditionally render load button if there are more available posts to fetch

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -34,7 +34,7 @@ const Index = () => {
         <p>Loading...</p>
       ) : (
         <Stack spacing={8}>
-          {data!.posts.map((post) => (
+          {data!.posts.posts.map((post) => (
             <Box key={post.id} p={5} shadow='md' borderWidth='1px'>
               <Heading fontSize='xl'>{post.title}</Heading>
               <Text mt={4}>{post.textSnippet}...</Text>
@@ -42,13 +42,13 @@ const Index = () => {
           ))}
         </Stack>
       )}
-      {data && (
+      {data && data.posts.hasMore && (
         <Flex>
           <Button 
             isLoading={fetching} 
             m='auto' 
             my={8} 
-            onClick={ () => setVariables({ limit: variables.limit, cursor: data.posts[data.posts.length - 1].createdAt })}
+            onClick={ () => setVariables({ limit: variables.limit, cursor: data.posts.posts[data.posts.posts.length - 1].createdAt })}
           >
             Load More
           </Button>

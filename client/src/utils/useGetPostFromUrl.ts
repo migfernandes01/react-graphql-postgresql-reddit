830b1c3b3494d98901bfa8a31d3f38df834d19b0
Miguel Fernandes <migamado01@gmail.com>
2022-06-05 17:38:01 -0400

    Use custom hook to get post id as integer from url

--- client/src/utils/useGetPostFromUrl.ts
+++ client/src/utils/useGetPostFromUrl.ts
@@ -1,13 +1,10 @@
-import { useRouter } from "next/router";
 import { usePostQuery } from "../generated/graphql";
+import { useGetIntId } from "./useGetIntId";
 
 // custom hook to get post id from url and execute post query passing the id as a variable
 export const useGetPostFromUrl = () => {
-    // initialize next router
-    const router = useRouter();
-
-    // parse id from query to int or set it to -1 if any error
-    const intId = typeof router.query.id === 'string' ? parseInt(router.query.id) : -1;
+    // use custom hook to get int Id from url
+    const intId = useGetIntId();
 
     // execute post query passing the id from query (get data and fetching back)
     return usePostQuery({

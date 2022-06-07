6c9c67f91c6af26a6fcc6902b79708943aa2f4d6
Miguel Fernandes <migamado01@gmail.com>
2022-06-07 19:54:21 -0400

    Create updoot data loader util function

---
+++ server/src/utils/createUpdootLoader.ts
@@ -0,0 +1,24 @@
+import DataLoader from "dataloader";
+import { Updoot } from "../entities/Updoot";
+import { User } from "../entities/User";
+
+// data loader for updoots
+// gets an array of objects with a postId and userId
+// return Updoot or null
+export const createUpdootLoader = () => new DataLoader<{postId: number, userId: number}, Updoot | null> (
+    async (keys) => {
+        // get all updoots using keys(array of objects with a postId and userId)
+        const updoots = await Updoot.findByIds(keys as any);
+
+        // create an object with string and Updoot
+        const updootIdsToUpdoot: Record<string, Updoot> = {};
+
+        // fill in updootIdsToUpdoot object with all userIds and postIds
+        updoots.forEach(updoot => {
+            updootIdsToUpdoot[`${updoot.userId}|${updoot.postId}`] = updoot;
+        });
+
+        // return object with userIds and postIds
+        return keys.map(key => updootIdsToUpdoot[`${key.userId}|${key.postId}`])
+    }
+); 
